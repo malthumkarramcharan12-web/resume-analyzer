fbad5e8d128ae51084aecb034c5cf22aad22050a
malthumkarramcharan12-web <malthumkarramcharan12@gmail.com>
2026-03-25 17:39:32 +0530

    Add skills list to skills.py

---
+++ skills.py
@@ -0,0 +1,6 @@
+SKILLS = [
+    "python", "java", "c++", "sql", "machine learning",
+    "deep learning", "nlp", "flask", "django",
+    "html", "css", "javascript", "react",
+    "node", "docker", "aws", "git"
+]
